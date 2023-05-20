3934d5e5e912b468e1a81bac965d12050097a5fb
Anthony Lines <linesanthony018@gmail.com>
2023-05-20 13:59:26 +0200

    json

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get("/", (req, res) =>
 app.get("/api/pokemons/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const pokemon = pokemons.find((pokemon) => pokemon.id === id);
-  res.send(`Vous avez demandé le pokémon ${pokemon.name}`);
+  res.json(pokemon);
 });
 
 // le nouveau point de terminaison , affichant le nombre total de pokémons :
