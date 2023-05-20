fb76ad670ab6a3795ddcc492f9b09df7338e3d85
Anthony Lines <linesanthony018@gmail.com>
2023-05-20 23:45:20 +0200

    clean

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ let pokemons = require("./mock-pokemon");
 const app = express();
 const port = 3000;
 
-app.use(favicon(__dirname + "/favicon.ico"));
-app.use(morgan("dev"));
+app.use(favicon(__dirname + "/favicon.ico")).use(morgan("dev"));
 
 app.get("/", (req, res) =>
   res.send("Hello, Express & Bienvenue dans ce pokédex!😎")
