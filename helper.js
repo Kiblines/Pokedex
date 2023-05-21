2eca06d98aca00dc39aac56222902142978affae
Anthony Lines <linesanthony018@gmail.com>
2023-05-21 17:43:03 +0200

    method getuniqueID

--- helper.js
+++ helper.js
@@ -4,3 +4,13 @@ exports.success = (message, data) => {
     data,
   };
 };
+
+//Le code exporte une fonction appelée "getUniqueId" qui prend en entrée un tableau d'objets Pokémon. Il extrait les identifiants des Pokémon à partir des objets et trouve l'identifiant maximal à l'aide de la méthode "reduce".
+//Ensuite, il calcule un identifiant unique en ajoutant 1 à l'identifiant maximal. Enfin, il renvoie l'identifiant unique.
+exports.getUniqueId = (pokemons) => {
+  const pokemonsIds = pokemons.map((pokemon) => pokemon.id);
+  const maxId = pokemonsIds.reduce((a, b) => Math.max(a, b));
+  const uniqueId = maxId + 1;
+
+  return uniqueId;
+};
